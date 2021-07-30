e64106306509eec953ae492f197b613f01563074
FsnRorschach <fabio_novais2010@hotmail.com>
2021-07-30 09:04:58 -0300

    Aula 4  - Firebase e Authentication

--- findfood/src/app/login-page/login-page.component.spec.ts
+++ findfood/src/app/login-page/login-page.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPageComponent } from './login-page.component';
 
 describe('LoginPageComponent', () => {
+  
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
 
@@ -22,4 +23,5 @@ describe('LoginPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
